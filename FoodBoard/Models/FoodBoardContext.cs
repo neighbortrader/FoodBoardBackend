e55125ed742a79e1a7adc0da8e985a4a8c33f1aa
mark.heimer <mark.heimer@t2-informatik.de>
2019-08-30 15:36:09 +0200

    more adjustings on implementing Identity

--- FoodBoard/Models/FoodBoardContext.cs
+++ FoodBoard/Models/FoodBoardContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FoodBoard.Models
 {
-    public class FoodBoardContext : IdentityDbContext<User>
+    public class FoodBoardContext : IdentityDbContext<LoginUser>
     {
         public FoodBoardContext(DbContextOptions<FoodBoardContext> options)
             : base(options)
