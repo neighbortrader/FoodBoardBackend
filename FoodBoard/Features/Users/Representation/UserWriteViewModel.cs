229b00de864b02afa781d47bac350ffacd23bf25
linde9821 <lindner197@gmail.com>
2019-09-10 09:41:20 +0200

    Changed all fields in User to string

--- FoodBoard/Features/Users/Representation/UserWriteViewModel.cs
+++ FoodBoard/Features/Users/Representation/UserWriteViewModel.cs
@@ -5,9 +5,9 @@
         public string Username { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
-        public int ZIPNumber { get; set; }
+        public string ZIPNumber { get; set; }
         public string Place { get; set; }
         public string Street { get; set; }
-        public int AddressNumber { get; set; }
+        public string AddressNumber { get; set; }
     }
 }
